83d6ab541ee70cfd19cb8c7af971181bcc902766
zeon0xx0 <jihyon5615@naver.com>
2026-05-25 18:19:03 +0900

    fix: 콜백 로그인 상태 처리 로직 수정

--- app/oauth/callback/client-processing/page.tsx
+++ app/oauth/callback/client-processing/page.tsx
@@ -1,17 +1,15 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useAuthStore } from '@/store/useAuthStore';
 import { useRouter } from 'next/navigation';
 
 export default function OauthHandlerPage() {
   const router = useRouter();
-  const { setIsLoggedIn } = useAuthStore();
 
   useEffect(() => {
-    setIsLoggedIn(true);
     router.replace('/');
-  }, [router, setIsLoggedIn]);
+  }, [router]);
+
   return (
     <div>
       <p>로그인 처리 중입니다...</p>
