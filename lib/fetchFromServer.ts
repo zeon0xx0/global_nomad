ea79b6b67f5e46eba35521ba15edc803673cd869
zeon0xx0 <jihyon5615@naver.com>
2026-05-25 19:54:28 +0900

    fix: env 숨기기

--- lib/fetchFromServer.ts
+++ lib/fetchFromServer.ts
@@ -27,11 +27,15 @@
 
 import { cookies } from 'next/headers';
 
-const baseUrl = process.env.BASE_URL || 'https://sp-globalnomad-api.vercel.app/14-2';
-
 export async function fetchFromServer(path: string, options: RequestInit = {}) {
   const cookieStore = await cookies();
   const accessToken = cookieStore.get('accessToken')?.value;
+
+  const baseUrl = process.env.BASE_URL;
+
+  if (!baseUrl) {
+    throw new Error('BASE_URL 환경변수가 설정되지 않았습니다.');
+  }
 
   const res = await fetch(`${baseUrl}${path}`, {
     ...options,
